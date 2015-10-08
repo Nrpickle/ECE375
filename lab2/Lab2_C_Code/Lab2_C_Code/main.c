43abfd9b60196a6adff2eb1dbf7beeeb4f2430e8
Nick McComb <nrpickle@gmail.com>
2015-10-07 19:02:08 -0700

    Finished challenge problem for lab 2

--- lab2/Lab2_C_Code/Lab2_C_Code/main.c
+++ lab2/Lab2_C_Code/Lab2_C_Code/main.c
@@ -3,16 +3,73 @@
  *
  * Created: 10/7/2015 6:10:21 PM
  * Author : nrpic_000
+ 
+ PORT MAP
+ Port B, Pin 4 -> Output -> Right Motor Enable
+ Port B, Pin 5 -> Output -> Right Motor Direction
+ Port B, Pin 7 -> Output -> Left Motor Enable
+ Port B, Pin 6 -> Output -> Left Motor Direction
+ Port D, Pin 1 -> Input -> Left Whisker
+ Port D, Pin 0 -> Input -> Right Whisker
+ 
  */ 
 
+#define F_CPU 16000000
 #include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+#define MOVE_FORWARD(void) (PORTB = 0b01100000)
+#define MOVE_REVERSE(void) (PORTB = 0b00000000)
+#define ROTATE_LEFT(void) (PORTB = 0b00100000)
+#define ROTATE_RIGHT(void) (PORTB = 0b01000000)
+
+#define CHECK_LEFT_WHISKER(void) (! (PIND & (1<<PD1)) )
+#define CHECK_RIGHT_WHISKER(void) (! (PIND & (1<<PD0)) )
+#define CHECK_BOTH_WHISKERS(void) (! (PIND & (1<<PD2)) )
 
 
 int main(void)
 {
+
+DDRB =  0b11110000;  //Set the upper nibble to outputs to drive the motors
+DDRD =  0b00000000;  //Set bits 0, 1, and 2 to inputs
+PORTB = 0b11110000;  //Set the default state of the motors
+
     /* Replace with your application code */
     while (1) 
     {
+		MOVE_FORWARD();
+		
+		/*
+		if ( PIND & (1<<PD0) ){
+			MOVE_FORWARD();
+		}
+		else {
+			MOVE_REVERSE();
+		}
+		*/
+		
+		if (CHECK_BOTH_WHISKERS()) {
+			_delay_ms(300);
+			MOVE_REVERSE();
+			_delay_ms(1000);
+		}
+		else if (CHECK_LEFT_WHISKER()){  //Check for left whisker
+			_delay_ms(300);
+			MOVE_REVERSE();
+			_delay_ms(300);
+			ROTATE_LEFT();
+			_delay_ms(500);
+		}
+		else if (CHECK_RIGHT_WHISKER()){  //Check for right whisker
+			_delay_ms(300);
+			MOVE_REVERSE();
+			_delay_ms(300);
+			ROTATE_RIGHT();
+			_delay_ms(500);
+		}
+		
     }
+	_delay_ms(50);
 }
-
