1db15cc33695ba5e3d514af44efdadc4eab106e6
Nick McComb <nrpickle@gmail.com>
2015-10-07 18:10:52 -0700

    Added lab 2 project

---
+++ lab2/Lab2_C_Code/Lab2_C_Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab2_C_Code.c
+ *
+ * Created: 10/7/2015 6:10:21 PM
+ * Author : nrpic_000
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
