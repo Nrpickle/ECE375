e9dd058b6ede5d69924f445bccab2ffe09e1d3fe
Nick <nick@nickmccomb.net>
2015-10-14 16:24:24 -0700

    Documentation changes

--- lab2/Lab2_C_Code/Lab2_C_Code/main.c
+++ lab2/Lab2_C_Code/Lab2_C_Code/main.c
@@ -2,7 +2,7 @@
  * Lab2_C_Code.c
  *
  * Created: 10/7/2015 6:10:21 PM
- * Author : nrpic_000
+ * Author : Nick McComb
  
  PORT MAP
  Port B, Pin 4 -> Output -> Right Motor Enable
@@ -19,6 +19,8 @@
 #include <util/delay.h>
 #include <stdio.h>
 
+/* These are the macros that are used to help ensure readability of code. They do not reduce execution time, because they are compiled into the code, not called as functions are. */
+
 #define MOVE_FORWARD(void) (PORTB = 0b01100000)
 #define MOVE_REVERSE(void) (PORTB = 0b00000000)
 #define ROTATE_LEFT(void) (PORTB = 0b00100000)
@@ -32,25 +34,16 @@
 int main(void)
 {
 
+//Initialize registers 
 DDRB =  0b11110000;  //Set the upper nibble to outputs to drive the motors
 DDRD =  0b00000000;  //Set bits 0, 1, and 2 to inputs
 PORTB = 0b11110000;  //Set the default state of the motors
 
-    /* Replace with your application code */
     while (1) 
     {
 		MOVE_FORWARD();
 		
-		/*
-		if ( PIND & (1<<PD0) ){
-			MOVE_FORWARD();
-		}
-		else {
-			MOVE_REVERSE();
-		}
-		*/
-		
-		if (CHECK_BOTH_WHISKERS()) {
+		if (CHECK_BOTH_WHISKERS()) {  //Check for "Both" whiskers, which is really just a button press
 			_delay_ms(300);
 			MOVE_REVERSE();
 			_delay_ms(1000);
